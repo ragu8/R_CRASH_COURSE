ea40b6524d2d15c1c76bfbd1fc6a2309a0aed55b
RAGUPATHI-M <ragumdr812@gmail.com>
2022-02-19 10:38:46 +0530

    R

---
+++ 01_Getting_Started.R
@@ -0,0 +1,45 @@
+#comments
+# This is a comment in R...  
+
+# working Directory
+getwd()
+# setwd("path")
+
+#Loading packages
+library("MASS")
+
+#Updating packages
+update.packages()
+
+#Help Files and Function Documentation
+?mean
+??"mean"
+
+#Conventions
+#Coding
+14/6
+
+for(myitem in 5:7){
+  cat("begins \n")
+  cat("current",myitem,"\n")
+  cat("ends \n\n")
+}
+
+
+ordfac.vec <- factor(x=c("Small","Large","Large","Regular","Small"),
+                     levels=c("Small","Regular","Large"),
+                     ordered=TRUE)
+
+#Math and Equation References
+x = 2
+y = 8
+x = y/4
+
+pi
+
+#Exercise
+#Say the word cat aloud.
+cat("cat aloud")
+
+#Using nothing but your brain,find the solution to 1+1.
+1+1
