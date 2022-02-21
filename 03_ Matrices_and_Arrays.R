004ddeecb09f29aac387431554cf724202a2aafa
RAGUPATHI-M <ragumdr812@gmail.com>
2022-02-21 18:00:56 +0530

    R

--- 03_ Matrices_and_Arrays.R
+++ 03_ Matrices_and_Arrays.R
@@ -148,3 +148,38 @@ A<-rbind(c(2,0,0,0),c(0,3,0,0),c(0,0,5,0),c(0,0,0,-1))
 A
 solve(A)%*%(A- diag(x=4))
 
+#Multidimensional Arrays
+AR<-array(data=1:24,dim=c(3,4,2))
+AR
+BR<- array(data=rep(1:24,times=4),dim=c(3,4,2,3))
+BR
+
+#Subsets,Extractions,and Replacements
+AR[2,,2]
+AR[1,, ]
+
+BR[1,,,1]
+BR[,,2,]
+BR[3:2,4,,]
+BR[2,,1,]
+
+#Exercise 3.3
+#Create and store a three-dimensional array with six layers of a 4 × 2 matrix, filled with a decreasing sequence of values between 4.8 and 0.1 of the appropriate length.
+A <- array(data=seq(from=4.8,to=0.1,length.out=24),dim=c(4,2,3)) 
+A
+
+# Extract and store as a new object the fourth- and first-row elements, in that order, of the second column only of all layers of (a).
+B <- A[c(4,1),2,]
+B
+
+#Use a fourfold repetition of the second row of the matrix formed in (b) to fill a new array of dimensions 2 × 2 × 2 × 3.
+C<- array(data=B[2,],dim=c(2,2,2,3))
+C
+
+# Create a new array comprised of the results of deleting the sixth layer of (a).
+D<- array(data=A[,,-3],dim=c(4,3,6))
+D
+
+#Overwrite the second and fourth row elements of the second column of layers 1, 3, and 5 of (d) with ???99.
+E <-array(data=(D[c(2,4),2,c(1,3,5)]),dim=c(4,3,6)) 
+E <- -99                 
