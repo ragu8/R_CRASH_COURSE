70e586bb0e387998818f4ccec4a75c9612741128
RAGUPATHI-M <ragumdr812@gmail.com>
2022-02-21 21:50:49 +0530

    R

--- 04_NON-NUMERIC_VALUES.R
+++ 04_NON-NUMERIC_VALUES.R
@@ -13,3 +13,20 @@ qux
 
 # A Logical Outcome:Relational Operators
 1==2
+1>2
+1!=2
+
+foo <- c(3,2,1,4,1,2,1,-1,0,3)
+bar <- c(4,1,2,1,1,0,0,3,0,4)
+length(foo) == length(bar)
+foo == bar
+foo<= bar
+
+foo.mat <- matrix(foo, nrow=5, ncol=2)
+bar.mar <- matrix(bar,nrow=5,ncol=2)
+foo.mat <= bar.mar
+
+qux <- foo == bar
+qux
+quux <- foo<=(bar+10)
+quusx
