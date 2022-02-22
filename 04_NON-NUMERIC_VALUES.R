abaa598df902aed9478e3f9d858b648fa5a000cf
RAGUPATHI-M <ragumdr812@gmail.com>
2022-02-22 20:36:52 +0530

    R

--- 04_NON-NUMERIC_VALUES.R
+++ 04_NON-NUMERIC_VALUES.R
@@ -40,3 +40,38 @@ a < 6+2
 a != 6
 
 #Create a new vector from the one used in (a) by deleting its first three elements. With this new vector, fill a 2 × 2 × 3 array. Examine the array for the following entries:
+
+b <- array(a[-1:-3],dim = c(2,2,3))
+b
+b<=(6/2)+4
+b<=((6/2)+4)+2 
+
+#Confirm the specific locations of elements equal to 0 in the 10 × 10 identity matrix I10 (see Section 3.3).
+
+#Multiple Comparison:Logical OPeration
+F||((T&&T)||F)
+!T && T
+(T&&(TRUE||F))&&F
+6||4
+6&&4
+c(1,2,3,4) || c(5,6,7,8)
+c(1,2,3,4) && c(5,6,7,8)
+c(1,2,3,4) | c(0,6,7,8)
+c(1,2,3,4) & c(0,6,7,8)
+
+# Logicals Are Numbers!
+
+TRUE + T
+F+F
+F-T
+
+1&&1
+1||1
+0&&1
+1&&1
+
+# Logical Subsetting and Extraction
+myvec <- c(5,-2.3,4,4,4,6,8,10,40)
+myvec[c(F,T,T,T,T,F,T,T,T,T)]            
+myvec<0
+myvec[myvec<0]
